925449d0c8c2a87e2a96a12637cc617abf9a75a4
KimeuLoise <kimeuloise@gmail.com>
2020-10-04 22:00:29 +0300

    content creation qt quote-form.component.html

--- src/app/quote-detail/quote-detail.component.ts
+++ src/app/quote-detail/quote-detail.component.ts
@@ -1,15 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Quote } from '../quote';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-quote-detail',
   templateUrl: './quote-detail.component.html',
-  styleUrls: ['./quote-detail.component.css']
+  styleUrls: ['./quote-detail.component.css'],
 })
 export class QuoteDetailComponent implements OnInit {
+  @Input() quote: Quote;
 
-  constructor() { }
+  @Output() deleteQuote = new EventEmitter<boolean>();
 
-  ngOnInit(): void {
+  deleteThisQuote(deleteThis: boolean) {
+    this.deleteQuote.emit(deleteThis);
   }
 
+
+  constructor() {}
+
+  ngOnInit(): void {}
 }
